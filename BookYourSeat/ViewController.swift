ab086c2e89c094299b7baa3c04ddd74bf3070635
georgekuttyme <georgekuttyme@gmail.com>
2019-01-10 08:28:43 +0530

    Initial Commit

---
+++ BookYourSeat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookYourSeat
+//
+//  Created by Georgekutty on 10/01/19.
+//  Copyright © 2019 Georgekutty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
